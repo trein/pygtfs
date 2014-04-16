70c4024c8af257a20dbb5204521279d5ca869f80
Guilherme M. Trein <guitrein@yahoo.com.br>
2014-04-16 11:11:40 -0400

    Add models to admin console

--- service/admin.py
+++ service/admin.py
@@ -1,3 +1,47 @@
+from django.contrib.gis import admin
 from django.contrib import admin
+from service.models import Agency
+from service.models import Zone
+from service.models import Stop
+from service.models import RouteType
+from service.models import Route
+from service.models import Service
+from service.models import Direction
+from service.models import Block
+from service.models import Shape
+from service.models import Trip
+from service.models import PickupType
+from service.models import DropOffType
+from service.models import StopTime
+from service.models import Calendar
+from service.models import ExceptionType
+from service.models import CalendarDate
+from service.models import Fare
+from service.models import PaymentMethod
+from service.models import FareAttribute
+from service.models import FareRule
+from service.models import Frequency
+from service.models import Transfer
 
-# Register your models here.
+admin.site.register(Stop, admin.OSMGeoAdmin)
+admin.site.register(Shape, admin.OSMGeoAdmin)
+admin.site.register(Agency)
+admin.site.register(Zone)
+admin.site.register(RouteType)
+admin.site.register(Route)
+admin.site.register(Service)
+admin.site.register(Direction)
+admin.site.register(Block)
+admin.site.register(Trip)
+admin.site.register(PickupType)
+admin.site.register(DropOffType)
+admin.site.register(StopTime)
+admin.site.register(Calendar)
+admin.site.register(ExceptionType)
+admin.site.register(CalendarDate)
+admin.site.register(Fare)
+admin.site.register(PaymentMethod)
+admin.site.register(FareAttribute)
+admin.site.register(FareRule)
+admin.site.register(Frequency)
+admin.site.register(Transfer)
